0a464030c816a339f4bb3fabd4ef5f070d7e1de0
sriramapadmanabhan <sriramsp.90@gmail.com>
2026-03-29 17:02:28 +0530

    langgraph implemented

--- RAG/pipeline.py
+++ RAG/pipeline.py
@@ -1,11 +1,13 @@
+
 from rag.RAG import C_rag
 from utils.pdf_loader import C_load_pdf
 from rag.index_creation import C_index
 from rag.mcp import C_mcp
 from rag.validation import C_validate
 
+
 class C_pipeline:
-    def pipeline(self,value,value2):
+    """def pipeline(self,value,value2):
         value.log.info('Inside pipeline')
         self.C_L_P=C_load_pdf()
         self.C_I=C_index(value)
@@ -40,5 +42,49 @@ class C_pipeline:
                 break
 
             if value.retry_count > 3:
-                break
+                break"""
 
+    def data_processing(self,value):
+        self.C_L_P = C_load_pdf()
+        self.C_L_P.load_pdf_context(value)
+        self.C_L_P.build_semantic_text(value)
+        self.C_L_P.split_into_chunks(value)
+
+    def index_processing(self,value):
+        self.C_I = C_index(value)
+        value.index_obj = self.C_I
+        self.C_I.create_faiss_index(value)
+        self.C_I.create_bm25_index(value)
+
+    def model_context_protocal(self,value,value2):
+        self.C_M = C_mcp()
+        self.C_M.create_MCP(value, value2)
+
+    def call_LLM(self,value,value2):
+        self.C_R = C_rag()
+        self.C_R.RAG(value, value2)
+
+    def validate_result(self,value,value2):
+        self.C_V = C_validate()
+        self.C_V.rag_validate(value, value2)
+
+    def conditional_check(self,value,value2):
+        if len(value.missed) > 0 or len(value.failed_validation) > 0:
+            value.log.info(f'validation failed Loop started for {value2.current_rag}')
+            value.log.info(f'retry count ==>{value.retry_count}')
+            value.log.info(f'Value Missed in LLM result {value.missed}')
+            value.log.info(f'value Failed in LLM result is {value.failed_validation}')
+            value.log.info(f'Semantic search used for above result is {value.L_semantic_search}')
+            value.retry_count += 1
+            if value.retry_count > 3:
+                return 'END'
+            return 'retry'
+        elif value.pipeline_step < 2:
+            value.pipeline_step = 2
+            value.retry_count = 0
+            value2.current_rag = value.rag_result['Document category']
+            value.log.info(f'Current Rag changed is {value2.current_rag}')
+            return 'retry'
+        else:
+            value.log.info(f'Program completed')
+            return 'END'
